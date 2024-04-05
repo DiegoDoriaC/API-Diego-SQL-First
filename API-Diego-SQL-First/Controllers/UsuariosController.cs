bdc9af14aac7ebed2c8610ffd2dc82b7ca0a18fd
DiegoDoria <retrexretrex@hotmail.com>
2024-04-04 22:01:52 -0500

    Primer Commit

---
+++ API-Diego-SQL-First/Controllers/UsuariosController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Diego_SQL_First.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+    }
+}
