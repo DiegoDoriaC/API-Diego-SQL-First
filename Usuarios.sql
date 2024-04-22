a98fb3ba73ae1788a80a01d0d361eae8cab686af
DiegoDoria <retrexretrex@hotmail.com>
2024-04-22 18:59:20 -0500

    Integracion de Put, Post y Delete; osea, se termino

--- Usuarios.sql
+++ Usuarios.sql
@@ -16,3 +16,46 @@ insert into Usuarios values
 	('Erica', 'Doria', 09),
 	('Elizabeth', 'Doria', 15)
 go
+
+
+----------LISTAR USUARIO----------
+select * from Usuarios
+go
+
+
+----------EDITAR USUARIO----------
+Create procedure sp_editarUsuario
+	@nombre varchar (20),
+	@apellido varchar (20),
+	@edad int
+as
+begin
+	update Usuarios set
+		apellido = @apellido,
+		edad = @edad
+		where nombre = @nombre
+end
+go
+
+
+----------CREAR USUARIO----------
+create procedure sp_crearUsuario
+	@nombre varchar (20),
+	@apellido varchar (20),
+	@edad int
+as
+begin
+	insert into Usuarios values
+		(@nombre, @apellido, @edad)
+end
+go
+
+
+----------ELIMINAR USUARIO----------
+create procedure sp_eliminarUsuario
+	@nombre varchar (20)
+as
+begin
+	delete from Usuarios where nombre = @nombre
+end
+go
